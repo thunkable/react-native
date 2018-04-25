07e9faac0365c7831b0aca826be6ce9766652734
James Ide <ide@expo.io>
2018-04-25 16:05:12 -0700

    [expo] Remove location requestAlwaysAuthorization code
    We don't run this code path; remove it so that static analyzers don't mistakenly infer we use this permission.

--- Libraries/Geolocation/RCTLocationObserver.m
+++ Libraries/Geolocation/RCTLocationObserver.m
@@ -234,10 +234,7 @@ RCT_EXPORT_METHOD(requestAuthorization)
     }
   }
   // Request location access permission
-  if ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"NSLocationAlwaysUsageDescription"] &&
-    [_locationManager respondsToSelector:@selector(requestAlwaysAuthorization)]) {
-    [_locationManager requestAlwaysAuthorization];
-  } else if ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"NSLocationWhenInUseUsageDescription"] &&
+  if ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"NSLocationWhenInUseUsageDescription"] &&
     [_locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
     [_locationManager requestWhenInUseAuthorization];
   }
